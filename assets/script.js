0e5e4a036847d82362fcc66965d4317599386a3e
b00kbaker <caimmurphy@gmail.com>
2021-01-18 20:28:16 -0700

    Designed each question as an object then defined a global array containing each object. Working towards a loop which can use an index format to populate the questions/answers.

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+var questionsArray= [questionOne, questionTwo, questionThree, questionFour, questionFive]
+
+var questionOne = {
+    question: "Which primitive data type is only defined as either true or false?",
+    options: "Boolean, String, Number",
+    correct: "Boolean"
+};
+
+var questionTwo = {
+    question: "Groups of data displayed within [] are called?",
+    options: "Strings, Arrays, Objects, Variables",
+    correct: "Arrays"
+};
+
+var questionThree = {
+    question: "Array indexes begin at '1'.",
+    options: "True, False",
+    correct: "False"
+};
+
+var questionFour = {
+    question: "A global variable is declared within---.",
+    options: "A method, A function, the Javascript file before functions, an array",
+    correct: "the Javascript file before functions"
+};
+
+var questionFive = {
+    question: "Which one is a property?",
+    options: "var chocolate, candy: caramel, console.log(Yum), (vanilla || chocolate)"
+};
+
+ 
+
+
